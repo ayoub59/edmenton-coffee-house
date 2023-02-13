568fbcd01286e85c9e6eaa560e5aa45e99fa87f5
ayoub zaanouni <ayoubzaanouni9@gmail.com>
2023-02-13 20:04:52 +0100

    finished the btn animation

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const arrow = document.querySelector("vectors-wrapper-3")
+const link = document.querySelector("link")
+const linkText = document.querySelector("text-3")
+
+
+
+link.addEventListener("click", (e) => {
+    console.log(e)
+})
